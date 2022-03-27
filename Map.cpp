4710a40642b7d76601d95e8f87684a5e2b354f7a
Masha <ponymasha@yandex.ru>
2022-03-27 11:56:11 +0300

    fix bugs

--- Map.cpp
+++ Map.cpp
@@ -18,64 +18,64 @@ int Map::GetNumberMap() {
 
 std::vector<String> Map::GetDiagramMap() {
 
-    std::vector<String> ZeroMap = {
-            "0000000000",
-            "0         0",
-            "0   s     0",
-            "0         0",
-            "0         0",
-            "0         0",
-            "0         0",
-            "0         0",
-            "0         0",
-            "0         0",
-            "0         0",
-            "00000000000",
-    };
-    std::vector<String> FirstMap = {
-            "s000000000000000000000000000000000000000",
-            "s                                      0",
-            "s   s                                  0",
-            "s                                      0",
-            "s                                      0",
-            "s                                      0",
-            "s                                      0",
-            "s                                      0",
-            "s                                      0",
-            "s                                      0",
-            "s                                      0",
-            "s                                      0",
-            "s                                      0",
-            "s                                      0",
-            "s                                      0",
-            "s                                      0",
-            "0                                      0",
-            "0                                      0",
-            "0                                      0",
-            "0                                      0",
-            "0                                      0",
-            "0                                      0",
-            "0                                      0",
-            "0                                      0",
-            "0000000000000000000000000000000000000000",
-    };
-    std::vector<String> TileMap = {
-            "@@@@@@@@@@@@@@@@@",
-            "@FFFFFFFFFFFFF@@@",
-            "@FFF4F4F4F4F4F@@@",
-            "@FFF4F4F4F4F4F@@@",
-            "@FFF4F494F4F4F@@@",
-            "@FFF4F3F3F4F4F@@@",
-            "@F3F3F1F1F3F3F@@@",
-            "@1F11F3F3F11F1@@@",
-            "@8F33F1F1F33F8@@@",
-            "@F1F1F444F1F1F@@@",
-            "@F4F4F4F4F4F4F@@@",
-            "@F4F4F3F3F4F4F@@@",
-            "@F4F4F?1?F4F4F@@@",
-            "@FFFFF0F2FFFFF@@@",
-            "@@@@@@@@@@@@@@@@@",
-    };
+//    std::vector<String> ZeroMap = {
+//            "0000000000",
+//            "0         0",
+//            "0   s     0",
+//            "0         0",
+//            "0         0",
+//            "0         0",
+//            "0         0",
+//            "0         0",
+//            "0         0",
+//            "0         0",
+//            "0         0",
+//            "00000000000",
+//    };
+//    std::vector<String> FirstMap = {
+//            "s000000000000000000000000000000000000000",
+//            "s                                      0",
+//            "s   s                                  0",
+//            "s                                      0",
+//            "s                                      0",
+//            "s                                      0",
+//            "s                                      0",
+//            "s                                      0",
+//            "s                                      0",
+//            "s                                      0",
+//            "s                                      0",
+//            "s                                      0",
+//            "s                                      0",
+//            "s                                      0",
+//            "s                                      0",
+//            "s                                      0",
+//            "0                                      0",
+//            "0                                      0",
+//            "0                                      0",
+//            "0                                      0",
+//            "0                                      0",
+//            "0                                      0",
+//            "0                                      0",
+//            "0                                      0",
+//            "0000000000000000000000000000000000000000",
+//    };
+//    std::vector<String> TileMap = {
+//            "@@@@@@@@@@@@@@@@@",
+//            "@FFFFFFFFFFFFF@@@",
+//            "@FFF4F4F4F4F4F@@@",
+//            "@FFF4F4F4F4F4F@@@",
+//            "@FFF4F494F4F4F@@@",
+//            "@FFF4F3F3F4F4F@@@",
+//            "@F3F3F1F1F3F3F@@@",
+//            "@1F11F3F3F11F1@@@",
+//            "@8F33F1F1F33F8@@@",
+//            "@F1F1F444F1F1F@@@",
+//            "@F4F4F4F4F4F4F@@@",
+//            "@F4F4F3F3F4F4F@@@",
+//            "@F4F4F?1?F4F4F@@@",
+//            "@FFFFF0F2FFFFF@@@",
+//            "@@@@@@@@@@@@@@@@@",
+//    };
 
     switch (number_map) {
         case 0:
@@ -89,106 +89,123 @@ std::vector<String> Map::GetDiagramMap() {
 void Map::SetDiagramMap(int i, int j, char texture) {
     switch (number_map) {
         case 0:
-             ZeroMap[i][j] = texture;
+            ZeroMap[i][j] = texture;
         case 1:
             FirstMap[i][j] = texture;
         case 2:
-             TileMap[i][j] = texture;
+            TileMap[i][j] = texture;
+    }
+}
+    void Map::CreateMap(std::vector<String> Diagram, int i, int j) {
+        if (Diagram[i][j] == ' ') map_sprite.setTextureRect(IntRect(160, 0, 32, 32));
+        if (Diagram[i][j] == 's') map_sprite.setTextureRect(IntRect(32, 0, 32, 32));
+        if (Diagram[i][j] == 'F') map_sprite.setTextureRect(IntRect(160, 0, 32, 32));
+        if (Diagram[i][j] == '0') map_sprite.setTextureRect(IntRect(0, 64, 32, 32));
+        if (Diagram[i][j] == '1') map_sprite.setTextureRect(IntRect(0, 128, 32, 32));
+        if (Diagram[i][j] == '2') map_sprite.setTextureRect(IntRect(0, 32, 32, 32));
+        if (Diagram[i][j] == '3') map_sprite.setTextureRect(IntRect(0, 96, 32, 32));
+        if (Diagram[i][j] == '4') map_sprite.setTextureRect(IntRect(0, 0, 32, 32));
+        if (Diagram[i][j] == '8') map_sprite.setTextureRect(IntRect(64, 96, 32, 32));
+        if (Diagram[i][j] == '?') map_sprite.setTextureRect(IntRect(160, 0, 32, 32));
+        if (Diagram[i][j] == '@') map_sprite.setTextureRect(IntRect(160, 128, 32, 32));
+        map_sprite.setPosition(j * 32, i *
+                                       32);//по сути раскидывает квадратики, превращая в карту. то есть задает каждому из них позицию. если убрать, то вся карта нарисуется в одном квадрате 32*32 и мы увидим один квадрат
+
     }
 
-void Map::CreateMap(std:: vector<String> Diagram, int i, int j) {
-    if (Diagram[i][j] == ' ') map_sprite.setTextureRect(IntRect(160,   0, 32, 32));
-    if (Diagram[i][j] == 's') map_sprite.setTextureRect(IntRect( 32,   0, 32, 32));
-    if (Diagram[i][j] == 'F') map_sprite.setTextureRect(IntRect(160,   0, 32, 32));
-    if (Diagram[i][j] == '0') map_sprite.setTextureRect(IntRect(  0,  64, 32, 32));
-    if (Diagram[i][j] == '1') map_sprite.setTextureRect(IntRect(  0, 128, 32, 32));
-    if (Diagram[i][j] == '2') map_sprite.setTextureRect(IntRect(  0,  32, 32, 32));
-    if (Diagram[i][j] == '3') map_sprite.setTextureRect(IntRect(  0,  96, 32, 32));
-    if (Diagram[i][j] == '4') map_sprite.setTextureRect(IntRect(  0,   0, 32, 32));
-    if (Diagram[i][j] == '8') map_sprite.setTextureRect(IntRect( 64,  96, 32, 32));
-    if (Diagram[i][j] == '?') map_sprite.setTextureRect(IntRect(160,   0, 32, 32));
-    if (Diagram[i][j] == '@') map_sprite.setTextureRect(IntRect(160, 128, 32, 32));
-    map_sprite.setPosition(j * 32, i * 32);//по сути раскидывает квадратики, превращая в карту. то есть задает каждому из них позицию. если убрать, то вся карта нарисуется в одном квадрате 32*32 и мы увидим один квадрат
-
-}
-
-Sprite Map::GetMapSprite() {
-    return map_sprite;
-}
+    Sprite Map::GetMapSprite() {
+        return map_sprite;
+    }
 ////////////////надо доделать
-void Map::InteractionTankWithMap(std::vector<String> Diagram, Player &tank) {
-    for (int i = tank.GetY() / 32; i < (tank.GetY() + tank.GetH()) /32; i++)//проходимся по тайликам, контактирующим с игроком, то есть по всем квадратикам размера 32*32, которые мы окрашивали в 9 уроке. про условия читайте ниже.
-        for (int j = tank.GetX() / 32; j < (tank.GetX() + tank.GetW()) /32; j++)//икс делим на 32, тем самым получаем левый квадратик, с которым персонаж соприкасается. (он ведь больше размера 32*32, поэтому может одновременно стоять на нескольких квадратах). А j<(x + w) / 32 - условие ограничения координат по иксу. то есть координата самого правого квадрата, который соприкасается с персонажем. таким образом идем в цикле слева направо по иксу, проходя по от левого квадрата (соприкасающегося с героем), до правого квадрата (соприкасающегося с героем)
-        {
-            if (tank.GetX() < 32) { tank.SetX(0); }
-            if (tank.GetY() < 32) { tank.SetY(0); }
-            if (tank.GetX() > 420) { tank.SetX(420); }
-            if (tank.GetY() > 422) { tank.SetY(390); }
-
-            if (Diagram[i][j] == '0' && tank.GetX() > j * 32 - 16 ){
-                if (tank.GetSpeedY() > 0) { tank.SetY(i * 32 - tank.GetH() - 6);}
-                if (tank.GetSpeedY() < 0) { tank.SetY(i * 32 + tank.GetH() + 6); }
-                if (tank.GetSpeedX() > 0) { tank.SetX(j * 32 + 16 - tank.GetW() - 6);}
-                if (tank.GetSpeedX() < 0) { tank.SetX(j * 32 + tank.GetW() + 6); }
-
-            }
-            if (Diagram[i][j] == '1'&& tank.GetY() > i * 32 - 16 ){
-                if (tank.GetSpeedY() > 0) { tank.SetY(i * 32 + 16 - tank.GetH() - 6);}
-                if (tank.GetSpeedY() < 0) { tank.SetY(i * 32 + tank.GetH() + 6); }
-                if (tank.GetSpeedX() > 0) { tank.SetX(j * 32 - tank.GetW() - 6);}
-                if (tank.GetSpeedX() < 0) { tank.SetX(j * 32 + tank.GetW() + 6); }
-            }
-            if (Diagram[i][j] == '2' && tank.GetX() < j * 32 + 16){
-                if (tank.GetSpeedY() > 0) { tank.SetY(i * 32 - tank.GetH() - 6);}
-                if (tank.GetSpeedY() < 0) { tank.SetY(i * 32 + tank.GetH() + 6); }
-                if (tank.GetSpeedX() > 0) { tank.SetX(j * 32 - tank.GetW() - 6);}
-                if (tank.GetSpeedX() < 0) { tank.SetX(j * 32 - 16 + tank.GetW() + 6); }
-
-            }
-            if (Diagram[i][j] == '3' && tank.GetY() < i * 32 + 16){
-                if (tank.GetSpeedY() > 0) { tank.SetY(i * 32 - tank.GetH() - 6);}
-                if (tank.GetSpeedY() < 0) { tank.SetY(i * 32 - 16 + tank.GetH() + 6); }
-                if (tank.GetSpeedX() > 0) { tank.SetX(j * 32 - tank.GetW() - 6);}
-                if (tank.GetSpeedX() < 0) { tank.SetX(j * 32  + tank.GetW() + 6); }
-            }
-
-            if (Diagram[i][j] == '4' )//если наш квадратик соответствует символу 0 (стена), то проверяем "направление скорости" персонажа:
+    void Map::InteractionTankWithMap(std::vector<String> Diagram, Player &tank) {
+        for (int i = tank.GetY() / 32; i < (tank.GetY() + tank.GetH()) /
+                                           32; i++)//проходимся по тайликам, контактирующим с игроком, то есть по всем квадратикам размера 32*32, которые мы окрашивали в 9 уроке. про условия читайте ниже.
+            for (int j = tank.GetX() / 32; j < (tank.GetX() + tank.GetW()) /
+                                               32; j++)//икс делим на 32, тем самым получаем левый квадратик, с которым персонаж соприкасается. (он ведь больше размера 32*32, поэтому может одновременно стоять на нескольких квадратах). А j<(x + w) / 32 - условие ограничения координат по иксу. то есть координата самого правого квадрата, который соприкасается с персонажем. таким образом идем в цикле слева направо по иксу, проходя по от левого квадрата (соприкасающегося с героем), до правого квадрата (соприкасающегося с героем)
             {
-                if (tank.GetSpeedY() > 0){//если мы шли вниз,
-                   tank.SetY(i * 32 - tank.GetH() - 6);}//то стопорим координату игрек персонажа. сначала получаем координату нашего квадратика на карте(стены) и затем вычитаем из высоты спрайта персонажа.
-                if (tank.GetSpeedY() < 0) {
-                    tank.SetY(i * 32 + tank.GetH() + 6);}//аналогично с ходьбой вверх. dy<0, значит мы идем вверх (вспоминаем координаты паинта)
-                if (tank.GetSpeedX() > 0) {
-                    tank.SetX(j * 32 - tank.GetW() - 6);}//если идем вправо, то координата Х равна стена (символ 0) минус ширина персонажа
-                if (tank.GetSpeedX() < 0) {
-                    tank.SetX(j * 32 + tank.GetW() + 6);}//аналогично идем влево
+                if (tank.GetX() < 32) { tank.SetX(32); }
+                if (tank.GetY() < 32) { tank.SetY(32); }
+                if (tank.GetX() > 420) { tank.SetX(420); }
+                if (tank.GetY() > 422) { tank.SetY(422); }
+
+                if (Diagram[i][j] == '0' && tank.GetX() > j * 32 - 16) {
+                    if (tank.GetSpeedY() > 0) { tank.SetY(i * 32 - tank.GetH() - 6); }
+                    if (tank.GetSpeedY() < 0) { tank.SetY(i * 32 + tank.GetH() + 6); }
+                    if (tank.GetSpeedX() > 0) { tank.SetX(j * 32 + 16 - tank.GetW() - 6); }
+                    if (tank.GetSpeedX() < 0) { tank.SetX(j * 32 + tank.GetW() + 6); }
+
+                }
+                if (Diagram[i][j] == '1' && tank.GetY() > i * 32 - 16) {
+                    if (tank.GetSpeedY() > 0) { tank.SetY(i * 32 + 16 - tank.GetH() - 6); }
+                    if (tank.GetSpeedY() < 0) { tank.SetY(i * 32 + tank.GetH() + 6); }
+                    if (tank.GetSpeedX() > 0) { tank.SetX(j * 32 - tank.GetW() - 6); }
+                    if (tank.GetSpeedX() < 0) { tank.SetX(j * 32 + tank.GetW() + 6); }
+                }
+                if (Diagram[i][j] == '2' && tank.GetX() < j * 32 + 16) {
+                    if (tank.GetSpeedY() > 0) { tank.SetY(i * 32 - tank.GetH() - 6); }
+                    if (tank.GetSpeedY() < 0) { tank.SetY(i * 32 + tank.GetH() + 6); }
+                    if (tank.GetSpeedX() > 0) { tank.SetX(j * 32 - tank.GetW() - 6); }
+                    if (tank.GetSpeedX() < 0) { tank.SetX(j * 32 - 16 + tank.GetW() + 6); }
+
+                }
+                if (Diagram[i][j] == '3' && tank.GetY() < i * 32 + 16) {
+                    if (tank.GetSpeedY() > 0) { tank.SetY(i * 32 - tank.GetH() - 6); }
+                    if (tank.GetSpeedY() < 0) { tank.SetY(i * 32 - 16 + tank.GetH() + 6); }
+                    if (tank.GetSpeedX() > 0) { tank.SetX(j * 32 - tank.GetW() - 6); }
+                    if (tank.GetSpeedX() < 0) { tank.SetX(j * 32 + tank.GetW() + 6); }
+                }
+
+                if (Diagram[i][j] ==
+                    '4')//если наш квадратик соответствует символу 0 (стена), то проверяем "направление скорости" персонажа:
+                {
+                    if (tank.GetSpeedY() > 0) {//если мы шли вниз,
+                        tank.SetY(i * 32 - tank.GetH() - 6);
+                    }//то стопорим координату игрек персонажа. сначала получаем координату нашего квадратика на карте(стены) и затем вычитаем из высоты спрайта персонажа.
+                    if (tank.GetSpeedY() < 0) {
+                        tank.SetY(i * 32 + tank.GetH() + 6);
+                    }//аналогично с ходьбой вверх. dy<0, значит мы идем вверх (вспоминаем координаты паинта)
+                    if (tank.GetSpeedX() > 0) {
+                        tank.SetX(j * 32 - tank.GetW() - 6);
+                    }//если идем вправо, то координата Х равна стена (символ 0) минус ширина персонажа
+                    if (tank.GetSpeedX() < 0) {
+                        tank.SetX(j * 32 + tank.GetW() + 6);
+                    }//аналогично идем влево
+                }
             }
-        }
-}
+    }
 
-bool Map::InteractionBulletWithMap(std::vector<String> Diagram, Bullet &bullet) {
-    for (int i = bullet.GetY() / 32; i < bullet.GetY() /32; i++)
-        for (int j = bullet.GetX() / 32; j < bullet.GetX()/32; j++){
-            if (bullet.GetX() < 0 || bullet.GetX() < 0 || bullet.GetX() > 435 || bullet.GetY() > 410  ) { return false;}
-            else {
-                if (Diagram[i][j] == '1'&& bullet.GetY() > i * 32 - 16 ){SetDiagramMap( i, j, 'F'); return false;}
-                if (Diagram[i][j] == '0' && bullet.GetX() > j * 32 - 16 ){SetDiagramMap( i, j, 'F'); return false;}
-                if (Diagram[i][j] == '2' && bullet.GetX() < j * 32 + 16){SetDiagramMap( i, j, 'F'); return false;}
-                if (Diagram[i][j] == '3' && bullet.GetY() < i * 32 + 16){SetDiagramMap( i, j, 'F'); return false;}
-                if (Diagram[i][j] == '4'){
-                    if (bullet.GetSpeedY() > 0){ SetDiagramMap( i, j, '1');}
-                    if (bullet.GetSpeedY() < 0){ SetDiagramMap( i, j, '3');}
-                    if (bullet.GetSpeedX() > 0){ SetDiagramMap( i, j, '0');}
-                    if (bullet.GetSpeedX() < 0){ SetDiagramMap( i, j, '2');}
-                    return false; }
-            return true; }
-        }
-}
-
-
-
-
-
-
+    bool Map::InteractionBulletWithMap(std::vector<String> Diagram, Bullet &bullet) {
+        for (int i = bullet.GetY() / 32; i < bullet.GetY() / 32; i++)
+            for (int j = bullet.GetX() / 32; j < bullet.GetX() / 32; j++) {
+                if (bullet.GetX() < 0 || bullet.GetX() < 0 || bullet.GetX() > 435 ||
+                    bullet.GetY() > 410) { return false; }
+                else {
+                    if (Diagram[i][j] == '1' && bullet.GetY() > i * 32 - 16) {
+                        SetDiagramMap(i, j, 'F');
+                        return false;
+                    }
+                    if (Diagram[i][j] == '0' && bullet.GetX() > j * 32 - 16) {
+                        SetDiagramMap(i, j, 'F');
+                        return false;
+                    }
+                    if (Diagram[i][j] == '2' && bullet.GetX() < j * 32 + 16) {
+                        SetDiagramMap(i, j, 'F');
+                        return false;
+                    }
+                    if (Diagram[i][j] == '3' && bullet.GetY() < i * 32 + 16) {
+                        SetDiagramMap(i, j, 'F');
+                        return false;
+                    }
+                    if (Diagram[i][j] == '4') {
+                        if (bullet.GetSpeedY() > 0) { SetDiagramMap(i, j, '1'); }
+                        if (bullet.GetSpeedY() < 0) { SetDiagramMap(i, j, '3'); }
+                        if (bullet.GetSpeedX() > 0) { SetDiagramMap(i, j, '0'); }
+                        if (bullet.GetSpeedX() < 0) { SetDiagramMap(i, j, '2'); }
+                        return false;
+                    }
+                    return true;
+                }
+            }
+    }
 
 
