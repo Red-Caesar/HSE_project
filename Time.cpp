87dde0435639da20f66d8636fe78323add92c3df
anastasiapple <nast-02@inbox.ru>
2022-03-28 13:38:58 +0300

    relocated values with time in files Game_time

--- Time.cpp
+++
@@ -1,5 +0,0 @@
-//
-// Created by NP on 28.03.2022.
-//
-
-#include "Time.h"
--- main.cpp
+++ main.cpp
@@ -4,105 +4,9 @@
 #include "Map.h"
 #include "Bullet.h"
 #include "Audio.h"
+#include "Game_time.h"
 using namespace sf;
 
-////////////////////////////////////////////////////КЛАСС ИГРОКА////////////////////////
-//class Bullet {// класс Игрока
-//public:
-//    float x, y, w, h, dx, dy, speed = 0; //координаты игрока х и у, высота ширина, ускорение (по х и по у), сама скорость
-//    int dir = 0; //направление (direction) движения игрока
-//    String File; //файл с расширением
-//    Image image;//сфмл изображение
-//    Texture texture;//сфмл текстура
-//    Sprite sprite;//сфмл спрайт
-//    bool Is_On_f = false;
-//
-//
-//    Bullet () {
-//        //File = F;//имя файла+расширение
-//        w = 7.0;
-//        h = 7.0;//высота и ширина
-//        //image.loadFromFile("..\\images/" + File);//запихиваем в image наше изображение вместо File мы передадим то, что пропишем при создании объекта. В нашем случае "hero.png" и получится запись идентичная 	image.loadFromFile("images/hero/png");
-//        //image.createMaskFromColor(Color(41, 33, 59));//убираем ненужный темно-синий цвет, эта тень мне показалась не красивой.
-//        //texture.loadFromImage(image);//закидываем наше изображение в текстуру
-//        //sprite.setTexture(texture);//заливаем спрайт текстурой
-//        x = 0;
-//        y = 0;//координата появления спрайта
-//        dir = 0;
-//        speed = 0.1;
-//        sprite.setTextureRect(IntRect(156, 27, w, h));  //Задаем спрайту один прямоугольник для вывода одного льва, а не кучи львов сразу. IntRect - приведение типов
-//    }
-//
-//
-//    Bullet (Player &player, String F){  //Конструктор с параметрами(формальными) для класса Bullet. При создании объекта класса мы будем задавать имя файла, координату Х и У, направление
-//        File = F;//имя файла+расширение
-//        //SetWidth(7.0);
-//        w = 7.0;
-//        h = 7.0;//высота и ширина
-//        image.loadFromFile("..\\images/" + File);//запихиваем в image наше изображение вместо File мы передадим то, что пропишем при создании объекта. В нашем случае "hero.png" и получится запись идентичная 	image.loadFromFile("images/hero/png");
-//        //image.createMaskFromColor(Color(41, 33, 59));//убираем ненужный темно-синий цвет, эта тень мне показалась не красивой.
-//        texture.loadFromImage(image);//закидываем наше изображение в текстуру
-//        sprite.setTexture(texture);//заливаем спрайт текстурой
-//        x = player.GetX();
-//        y = player.GetY();//координата появления спрайта
-//        dir = player.GetDir();
-//        speed = 0.1;
-//        sprite.setTextureRect(IntRect(156, 27, w,h));  //Задаем спрайту один прямоугольник для вывода одного льва, а не кучи львов сразу. IntRect - приведение типов
-//    }
-//
-//
-//    void update(float time) //функция "оживления" объекта класса. update - обновление. принимает в себя время SFML , вследствие чего работает бесконечно, давая персонажу движение.
-//    {
-//        switch (dir)//реализуем поведение в зависимости от направления. (каждая цифра соответствует направлению)
-//        {
-//            case 0:
-//                dx = speed;
-//                dy = 0;
-//                break;//по иксу задаем положительную скорость, по игреку зануляем. получаем, что персонаж идет только вправо
-//            case 1:
-//                dx = -speed;
-//                dy = 0;
-//                break;//по иксу задаем отрицательную скорость, по игреку зануляем. получается, что персонаж идет только влево
-//            case 2:
-//                dx = 0;
-//                dy = speed;
-//                break;//по иксу задаем нулевое значение, по игреку положительное. получается, что персонаж идет только вниз
-//            case 3:
-//                dx = 0;
-//                dy = -speed;
-//                break;//по иксу задаем нулевое значение, по игреку отрицательное. получается, что персонаж идет только вверх
-//        }
-//
-//
-//
-//        x += dx * time;//то движение из прошлого урока. наше ускорение на время получаем смещение координат и как следствие движение
-//        y += dy * time;//аналогично по игреку
-//
-//        if(x>=32*14||y>=32*13||x<=0||y<=0){
-//            speed = 0;
-//            Is_On_f = false;
-//        }
-//        else{
-//            sprite.setPosition(x, y);
-//        }
-//        //speed = 0;//зануляем скорость, чтобы персонаж остановился.
-//        //выводим спрайт в позицию x y , посередине. бесконечно выводим в этой функции, иначе бы наш спрайт стоял на месте.
-//    }
-//
-//    void New_Coordinates_and_Dir(Player &player){
-//        dir = player.GetDir();
-//        x = player.GetX() + 10;
-//        y = player.GetY() + 10;
-//        speed = 0.1;
-//    }
-//
-//    void SetFile(String F){
-//        File = F;
-//        image.loadFromFile("..\\images/" + File);
-//        texture.loadFromImage(image);
-//        sprite.setTexture(texture);
-//    }
-//};
 
 int main() {
 
@@ -113,6 +17,8 @@ int main() {
     ///music
 
     Audio audio;
+    Game_time g_time;
+
     audio.Init();
     audio.playGame();
     audio.playSpawn();
@@ -124,9 +30,6 @@ int main() {
     bool BigFlag = false;
     float CurrentFrame = 0;//хранит текущий кадр
 
-    Clock clock;
-
-
 
     int n_bul = 1;
     Bullet bul[n_bul];
@@ -135,9 +38,8 @@ int main() {
     }
 
     while (window.isOpen()) {
-        float time = clock.getElapsedTime().asMicroseconds();
-        clock.restart();
-        time = time /1000;
+          g_time.Init();
+
         // Обрабатываем очередь событий в цикле
         Event event;
 
@@ -165,7 +67,7 @@ int main() {
             NewBullet = false;
         }
         NewBullet = false;
-        tank.update(time);
+        tank.update(g_time.GetTime());
         map.InteractionTankWithMap(map.GetDiagramMap(), tank);
         window.clear();
 
@@ -181,7 +83,7 @@ int main() {
 
         for (int i = 0; i < n_bul; i++) {
             if (bul[i].Is_On_f) {
-                bul[i].update(time);
+                bul[i].update(g_time.GetTime());
                 bul[i].Is_On_f = map.InteractionBulletWithMap(map.GetDiagramMap(),bul[i]);
                 window.draw(bul[i].sprite);//рисуем спрайт пули
             }
